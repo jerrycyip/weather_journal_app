da5b60a38d97fdde11c7eec38e68d1f9f87c6cf1
Jerry Yip <jerry.yip@gmail.com>
2020-09-16 19:19:29 -0700

    initial commit

---
+++ website/app.js
@@ -0,0 +1,41 @@
+/* Global Variables */
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+// api constructors for retrieving weather data from openweathermap.org
+const apiKey = '2ad76e8d73c762b9428f448e8ecc3119';
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+
+// event listener that triggers new weather journal entry
+document.getElementById('generate').addEventListener('click', newEntry);
+
+// add entry to weather journal
+function newEntry(event) {
+    // select the input values
+    const zipCode = document.getElementById('zip').value;
+    const currentMood = document.getElementById('feelings').value;
+    // api GET call to openweathermap api with zip code
+    getWeather(baseURL, zipCode, apiKey);
+    // if API call is successful, POST weather data to server
+    //.then
+}
+
+// retrieve weather data based on US zip code
+const getWeather = async (baseURL, zip, apiKey) => {
+    // GET fetch request to openweathermap.org API
+    const res = await fetch(baseURL + zip + '&appid=' + apiKey)
+    // if successful, log and return weather data
+    try {
+        const weatherData = res.json();
+        console.log(weatherData);
+        
+        return weatherData;
+    } 
+    catch (error) {
+        console.log("error occured:", error);
+    }
+}
+
+
