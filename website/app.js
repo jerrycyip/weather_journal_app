f14cc74adee5fb4bed19f8be63fa720844adb5f2
Jerry Yip <jerry.yip@gmail.com>
2020-09-21 18:15:26 -0700

    feat: implement viewing latest entry

--- website/app.js
+++ website/app.js
@@ -7,6 +7,8 @@ let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
 // api constructors for retrieving weather data from openweathermap.org
 const apiKey = '2ad76e8d73c762b9428f448e8ecc3119';
 const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+const units = '&units=imperial';
+
 // weather icon = URL plus + iconID + '@2x.png'
 const iconURL = 'http://openweathermap.org/img/wn/'; 
 const iconFormat = '@2x.png'
@@ -18,9 +20,10 @@ document.getElementById('generate').addEventListener('click', newEntry);
 function newEntry(event) {
     // select the input values
     const zipCode = document.getElementById('zip').value;
+
     const currentMood = document.getElementById('feelings').value;
     // api GET call to openweathermap api with zip code
-    getWeather(baseURL, zipCode, apiKey)
+    getWeather(baseURL, zipCode, apiKey, units)
     // if API call is successful, POST weather data to server
     .then (function (weatherData){
         postEntry('/add', {date: newDate, weather: weatherData.weather[0]['description'], 
@@ -33,9 +36,9 @@ function newEntry(event) {
 }
 
 // retrieve weather data based on US zip code
-const getWeather = async (baseURL, zip, apiKey) => {
+const getWeather = async (baseURL, zip, apiKey, units) => {
     // GET fetch request to openweathermap.org API
-    const res = await fetch(baseURL + zip + '&appid=' + apiKey)
+    const res = await fetch(baseURL + zip + units + '&appid=' + apiKey )
     // if successful, log and return weather data
     try {
         const weatherData = await res.json(); 
@@ -82,6 +85,13 @@ const updateUI = async(url='') => {
     })
     try {
         allEntries = await res.json();
+        latestEntry = allEntries[allEntries.length-1];
+        weatherIcon = latestEntry.icon;
+        document.getElementById('date').innerHTML = latestEntry.date;
+        document.getElementById('temp').innerHTML = latestEntry.temp;
+        document.getElementById('weather').innerHTML = latestEntry.weather;
+        document.querySelector('.weather-icon').innerHTML = `<img src="${iconURL}${weatherIcon}${iconFormat}" alt="weather-icon"> </img>`;
+        document.getElementById('content').innerHTML = latestEntry.mood;
         console.log(allEntries);
     }
     catch (error) {
